699685e5c530dd02e6e40f80fcbc604d0f0d235c
Natalie <kinern01@gmail.com>
2020-03-13 10:31:16 -0700

    Updates

---
+++ precache-manifest.9c1d03e11a0d813495d357473b8693e0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f399799eb6ce8916728dd206cd8cc037",
+    "url": "/kinern-portfolio/index.html"
+  },
+  {
+    "revision": "f2c19842a9492175e053",
+    "url": "/kinern-portfolio/static/css/2.2bc3d380.chunk.css"
+  },
+  {
+    "revision": "1f1eaee80c4571ad684c",
+    "url": "/kinern-portfolio/static/css/main.e4fe5c22.chunk.css"
+  },
+  {
+    "revision": "f2c19842a9492175e053",
+    "url": "/kinern-portfolio/static/js/2.da9d6f73.chunk.js"
+  },
+  {
+    "revision": "a1e12f97867665430e5c8e39fc9f180a",
+    "url": "/kinern-portfolio/static/js/2.da9d6f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1eaee80c4571ad684c",
+    "url": "/kinern-portfolio/static/js/main.98f24245.chunk.js"
+  },
+  {
+    "revision": "74b615b652a8c0168327",
+    "url": "/kinern-portfolio/static/js/runtime-main.69768d43.js"
+  },
+  {
+    "revision": "450eb05cc61473a9a9880e4ace608e2f",
+    "url": "/kinern-portfolio/static/media/butterfly.450eb05c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/kinern-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/kinern-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/kinern-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/kinern-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/kinern-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b1f5760200e9cc7b9bc2ac722c27dbc4",
+    "url": "/kinern-portfolio/static/media/frog.b1f57602.jpg"
+  },
+  {
+    "revision": "9b2ba8850d9f5d1fca713d0b2c14a5d7",
+    "url": "/kinern-portfolio/static/media/lion-dog-statue.9b2ba885.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kinern-portfolio/precache-manifest.c1c820fc8ca7790f2a8f6ed98a2a366b.js"
+  "/kinern-portfolio/precache-manifest.9c1d03e11a0d813495d357473b8693e0.js"
 );
 
 self.addEventListener('message', (event) => {
