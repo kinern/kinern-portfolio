d95215f9b15d3e5239ea0f13c26876a6fdeabef5
Natalie <kinern01@gmail.com>
2020-03-21 16:23:42 -0700

    switched BrowserRouter to MemoryRouter

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <MemoryRouter>
         <App />
-    </BrowserRouter>
+    </MemoryRouter>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
