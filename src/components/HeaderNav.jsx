3e18f46214939dc679bca967dbdd0f2d0834ed69
Natalie <kinern01@gmail.com>
2020-08-26 23:34:26 -0700

    removing broken image in navigation.

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -6,9 +6,7 @@ export default () => {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <a className="navbar-brand" href="#top">
-                <img src="" width="30" height="30" alt="" />Natalie Kiner
-            </a>
+            <a className="navbar-brand" href="#top">Natalie Kiner</a>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="nav navbar-nav">
                     <li className="nav nav-item">
