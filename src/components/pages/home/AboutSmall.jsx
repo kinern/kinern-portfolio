e8a4d6320bdde6ae8205fdac2652fbe121de1169
Natalie <kinern01@gmail.com>
2021-02-22 11:11:58 -0800

    more styling adjustments for small screen sizes.

--- src/components/pages/home/AboutSmall.jsx
+++ src/components/pages/home/AboutSmall.jsx
@@ -12,7 +12,10 @@ const AboutSmall = () => {
                 </div>
                 <div className="right-col">
                     <p>
-                        My name is Natalie Kiner. I am a Full Stack Web Developer living in the Pacific Northwest. I have worked on a number of web applications, including the multilingual blogging website Bloguru and CRM suite PSP Pilot. Along with my professional work, I enjoy learning about the latest technologies and building personal apps. My latest project is JTrack, a fitness tracking mobile application.
+                        My name is Natalie Kiner. I am a Full Stack Web Developer living in the Pacific Northwest. I have worked on a number of web applications, including the multilingual blogging website Bloguru and CRM suite PSP Pilot. 
+                        <br />
+                        <br />
+                        Along with my professional work, I enjoy learning about the latest technologies and building personal apps. My latest project is JTrack, a fitness tracking mobile application.
                         <br />
                         <br />
                         I am also passionate about creating art. My main mediums are pens and watercolor paints. You can see a glimpse of artwork on my Instagram page.
